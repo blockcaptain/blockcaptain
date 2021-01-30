cf255c2beefffd4a71220ac6f6631dbca83d57ae
Will Brown <will@rebeagle.com>
2021-01-30 17:28:33 +0000

    build change

--- eng/ci/integration-run.sh
+++ eng/ci/integration-run.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 mkdir -p target/debian
 wget "https://github.com/blockcaptain/restic/releases/latest/download/restic" -O target/debian/restic
-blkcapteng test --container
+sudo blkcapteng test --container
